c70d07b282dc969227a4dd2da39bf9abc9057420
DuHuaL <469580396@qq.com>
2020-09-12 17:59:09 +0800

    打包字体

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,13 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif|webp)$/,
         use: ['file-loader']
+      },
+      // 加载字体
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
       }
     ]
   },
